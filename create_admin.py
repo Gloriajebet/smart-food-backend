723669193b0f8ce29a64e424b3764d21ffcad001
Gloria Jebet <glorianushka@gmail.com>
2026-07-20 14:39:07 +0300

    Add admin creation password

--- create_admin.py
+++ create_admin.py
@@ -7,9 +7,9 @@ django.setup()
 
 from django.contrib.auth.models import User
 
-username = "admin"
-email = "admin@gmail.com"
-password = "Admin12345"
+username = "Gloria"
+email = "glorianushka@gmail.com"
+password = "GLOOisaBaddie"
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(
