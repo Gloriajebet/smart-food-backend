daaab72f8f60ef3eae30fc810618d8f2f4de81cb
Gloria Jebet <glorianushka@gmail.com>
2026-08-03 23:31:46 +0300

    Update setting.py

--- backend/settings.py
+++ backend/settings.py
@@ -137,8 +137,9 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "glorianushka@gmail.com"
-EMAIL_HOST_PASSWORD = "lpycyglzchejhdjj"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
